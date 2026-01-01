bdf7a7d861a3e7548bc441bea955fc37e6ef9c39
amanibhr <amani.bouhrira@esprit.tn>
2026-01-01 13:14:55 +0100

    add files backend for chatbot and other

--- backend-mobile/ekrili/feedback/urls.py
+++ backend-mobile/ekrili/feedback/urls.py
@@ -1,19 +1,12 @@
 from django.urls import path
 from . import views
-
+ 
 urlpatterns = [
-    path('getFeedback', views.getAllFeedback, name="feedback"),
-    path('feedback/<int:id>', views.getFeedbackById, name="feedback"),
-    path('feedback', views.addFeedback, name="feedback"),
-    path('feedback/<int:id>', views.updateFeedback, name="feedback"),
-    path('feedback/<int:id>', views.deleteFeedback, name="deleteFeedback"),
+    path('getFeedback', views.getAllFeedback, name="getFeedback"),
+    path('getFeedbackById/<int:id>', views.getFeedbackById, name="getFeedbackById"),
+    path('addFeedback', views.addFeedback, name="addFeedback"),
+    path('updateFeedback/<int:id>', views.updateFeedback, name="updateFeedback"),
+    path('addLikeDislike/<int:id>', views.addLikeDislike, name="addLikeDislike"),
+    path('deleteFeedback/<int:id>', views.deleteFeedback, name="deleteFeedback"),
     ################################################################
-   
-    
-  
-    
-    
-    
-    
-
 ]
