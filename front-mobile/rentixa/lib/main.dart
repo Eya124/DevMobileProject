b49307deb6f0bdf6edb9734e6f181eb7a12d05a6
amanibhr <amani.bouhrira@esprit.tn>
2026-01-23 12:47:08 +0100

    bugfix to run project

--- front-mobile/rentixa/lib/main.dart
+++ front-mobile/rentixa/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:rentixa/admin/admin_panel.dart';
 
 // --- Providers ---
 import 'package:rentixa/providers/auth_provider.dart';
